f55d5c94f87c2cf00d7eb5ba8e9b8f33406c8134
Volodymyr Voronkin <voronkinvladimir328@gmail.com>
2025-04-11 21:21:18 +0200

    added hero

--- src/app/[locale]/(pages)/page.tsx
+++ src/app/[locale]/(pages)/page.tsx
@@ -1,11 +1,11 @@
-
+import Hero from "@/app/components/Hero/Hero";
 
 
 export default function HomePage() {
 
   return (
-    <div>
-
+    <div className="relative z-10">
+      <Hero/>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    screens: {
+      'sss': '360px',
+      'ss': '414px',
+      's': '540px',
+      'xxm': '768px',
+      'xm':'1024px',
+      'm': '1280px',
+      'l': '1440px',
+      'xl': '1920px',
+    }
   },
   plugins: [],
 } satisfies Config;
