274216c272bee6d808038b03aa1ae3537bcade98
Volodymyr Voronkin <voronkinvladimir328@gmail.com>
2025-04-12 02:56:19 +0200

    .

--- src/app/components/Commissions/Commissions.tsx
+++ src/app/components/Commissions/Commissions.tsx
@@ -17,7 +17,7 @@ const Commissions = () => {
     ]
     return (
         <div className={`relative`}>
-            <Image src={`/images/commissionsglow.png`} width={1502} height={1424} alt={`glow`} className={`absolute top-[-300px] right-0 l:w-full m:max-h-[2000px] object-cover m:top-[-96px]`}/>
+            <Image src={`/images/commissionsglow.png`} width={1502} height={1424} alt={`glow`} className={`absolute top-[-300px] right-0 l:w-full max-h-[1300px] object-fill m:top-[-96px]`}/>
             <div className={`w-full bg-[#EFC653] rounded-[24px] relative z-100 px-4 py-6 xxm:x-8 xxm:py-8 m:py-16 m:px-16 l:py-[64px] l:px-[124px] mb-[40px] xxm:mb-[96px]`}>
                 <div className={`flex gap-3 items-center mb-6`}>
                     <span><Image src={`/icons/wallet.png`} alt={`wallet`} width={42} height={42} className={`xxm:w-[62px] xxm:h-[62px]`}/> </span>
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,10 +6,32 @@ interface Links {
 }
 
 const Footer: NextComponentType<NextPageContext, {}> = () => {
-
+const links = [
+    {
+        label: 'Terms & Conditions',
+        link: '#'
+    },
+    {
+        label: "Privacy Policy",
+        link: '#'
+    },
+    {
+        label: 'Cookie Policy',
+        link: '#'
+    }
+]
     return (
-        <footer >
-
+        <footer className={`bg-[#12110E] relative z-10 p-8 text-white`}>
+            <div className={`flex flex-col gap-6 items-center mb-[48px] footer-block relative xxm:flex-row xxm:justify-center`}>
+                {links.map((item) => (
+                    <a href={item.link} key={item.label}> {item.label} </a>
+                ))}
+            </div>
+            <div className={`text-[#8D8D8D]`}>
+                <p className={`text-center`}>
+                    © Dreamplay Aff. All rights reserved.
+                </p>
+            </div>
         </footer>
     );
 };
